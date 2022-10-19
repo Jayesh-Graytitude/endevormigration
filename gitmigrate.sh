1142d64e81e74cf1ba5e8b1131f062f6126a008f
Jayesh-Graytitude <jayesh.patel@graytitude.com>
2022-10-19 19:52:56 +0530

    Update gitmigrate.sh

--- gitmigrate.sh
+++ gitmigrate.sh
@@ -129,7 +129,7 @@ fi
 echo "** Clonning new git repository to USS"
 echo ''
 #
-git clone -q $NewRepoUrl $ussgitpath
+git clone -q "$NewRepoUrl" "$ussgitpath"
 #
 #############################################################
 # This step triggers migration process for the application. #
